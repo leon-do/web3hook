68c1f927ddc9d0d4666bbbe91cd7ee17983ac471
leon-do <leondo123@gmail.com>
2023-01-26 21:56:14 -0500

    query all

--- src/pages/api/evm/index.ts
+++ src/pages/api/evm/index.ts
@@ -1,9 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+import { Trigger } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 type Data = {
-  success: boolean;
+  success: Trigger[];
 };
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  res.status(200).json({ success: true });
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const x = await prisma.trigger.findMany({ where: {} });
+  console.log(x);
+
+  res.status(200).json({ success: x });
 }
