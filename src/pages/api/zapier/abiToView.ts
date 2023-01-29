4f59d5b389345d4cb6ae59f020df025fdcc03f13
leon-do <leondo123@gmail.com>
2023-01-28 23:21:53 -0500

    update abiToView

--- src/pages/api/zapier/abiToView.ts
+++ src/pages/api/zapier/abiToView.ts
@@ -1,34 +1,54 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { ethers } from "ethers";
-import { PrismaClient } from "@prisma/client";
-import { Raleway } from "@next/font/google";
 
-const prisma = new PrismaClient();
-
-type Data = {
+// { key: 'Method', children: [ { key: 'balanceOf_account', type: 'string', helpText: "address" }]}
+type View = {
   key: string;
-  choices: string[];
+  children: {
+    key: string;
+    type: string;
+    helpText: string;
+  }[];
 };
+
+type Error = {};
 
 type ABI = {
   stateMutability: string;
   name: string;
+  inputs: {
+    type: string;
+    name: string;
+  }[];
 };
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<View | Error>) {
   try {
     console.log("/zapier/abiToView");
     const abi: ABI[] = JSON.parse(req.body.abi);
-    // filter out view function names
-    const views: string[] = abi.filter((item) => item.stateMutability === "view").map((item) => item.name);
-    return res.status(200).send({ key: "view", choices: views });
+    const view: View = getView(abi);
+    return res.status(200).send(view);
   } catch (err) {
     console.log(err);
-    return res.status(400).send({ key: "view", choices: [] });
+    return res.status(400).send({});
   }
 }
 
-function abiToView(_abi: ABI[]): Data {
-  const views: string[] = _abi.filter((item) => item.stateMutability === "view").map((item) => item.name);
-  return { key: "view", choices: views };
+// { key: 'Method', children: [ { key: 'balanceOf_account', type: 'string', helpText: "address" }]}
+function getView(_abi: ABI[]): View {
+  const response: View = { key: "methods", children: [] };
+  const views = _abi.filter((item) => item.stateMutability === "view");
+  for (let view of views) {
+    response.children.push({
+      key: view.name,
+      type: "copy",
+      helpText: view.name,
+    });
+    const child = view.inputs.map((input) => ({
+      key: `${view.name}_${input.name}_${input.type}`,
+      type: "string",
+      helpText: input.type,
+    }));
+    response.children.push(...child);
+  }
+  return response;
 }
