fe00338908ec29751ac95f5b7194d70425cb3430
leon-do <leondo123@gmail.com>
2023-02-04 05:26:32 -0500

    github auth

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["user-images.githubusercontent.com", "lh3.googleusercontent.com"],
+    domains: ["user-images.githubusercontent.com", "lh3.googleusercontent.com", "avatars.githubusercontent.com"],
   },
 };
 
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,7 +2,7 @@ import NextAuth, { NextAuthOptions, Session, User } from "next-auth";
 import * as Prisma from "@prisma/client";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GoogleProvider from "next-auth/providers/google";
-import GithubProvider from "next-auth/providers/github";
+import GitHubProvider from "next-auth/providers/github";
 
 const prisma = new Prisma.PrismaClient();
 
@@ -13,6 +13,10 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_ID as string,
       clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
     }),
   ],
   theme: {
