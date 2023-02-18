4e3a223f2f3228d5bb678ef14b04ff7bfa100a04
leon-do <leondo123@gmail.com>
2023-02-17 23:58:32 -0500

    update schema required name and email

--- src/pages/api/zapier/auth.ts
+++ src/pages/api/zapier/auth.ts
@@ -4,7 +4,8 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 type Data = {
-  success: boolean;
+  name: string;
+  email: string;
 };
 
 // https://platform.zapier.com/docs/apikey
@@ -12,12 +13,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   try {
     console.log("/zapier/auth");
     const { api_key } = req.body;
-    if (!api_key) return res.status(401).json({ success: false });
+    if (!api_key) return res.status(401).json({ name: "", email: "" });
     const user = await prisma.user.findUnique({ where: { apiKey: api_key } });
-    if (user) return res.status(200).json({ success: true });
-    return res.status(400).json({ success: false });
+    if (user) return res.status(200).json({ name: user.name, email: user.email });
+    return res.status(400).json({ name: "", email: "" });
   } catch (error) {
     console.error(error);
-    return res.status(400).json({ success: false });
+    return res.status(400).json({ name: "", email: "" });
   }
 }
