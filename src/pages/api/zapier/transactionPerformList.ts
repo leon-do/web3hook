c2c999f8ae27faf5228a0c96bb19e5169ee900df
leon-do <leondo123@gmail.com>
2023-02-22 05:36:05 -0500

    test value 100

--- src/pages/api/zapier/transactionPerformList.ts
+++ src/pages/api/zapier/transactionPerformList.ts
@@ -23,7 +23,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
       transactionHash: "0x0",
       fromAddress: "0x0",
       toAddress: "0x0",
-      value: "0",
+      value: "100",
       chainId: 1,
       data: "0x0",
       gasLimit: "21000",
