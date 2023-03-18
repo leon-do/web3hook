2e9b958b43a33f9e5e09b2d4918296d9fa67dc3a
leon-do <leondo123@gmail.com>
2023-03-18 14:55:32 -0400

    canto testnet

--- src/pages/api/zapier/callViewFunction.ts
+++ src/pages/api/zapier/callViewFunction.ts
@@ -22,6 +22,7 @@ const providers: Providers = {
   "42161": new ethers.providers.JsonRpcProvider("https://arb1.arbitrum.io/rpc"),
   "5001": new ethers.providers.JsonRpcProvider("https://rpc.testnet.mantle.xyz"),
   "10": new ethers.providers.JsonRpcProvider("https://rpc.ankr.com/optimism"),
+  "7701": new ethers.providers.JsonRpcProvider("https://canto-testnet.plexnode.wtf"),
 };
 
 type Providers = {
